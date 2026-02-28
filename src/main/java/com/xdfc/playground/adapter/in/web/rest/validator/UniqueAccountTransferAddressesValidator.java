d87df6e6d7e6a8c1e9e988d6ab40e983c7b9faf7
0xdfc <invalid@gg.cn>
2026-02-28 13:48:32 +0400

    chore: add comment regarding suboptimal validator that should be improved later

--- src/main/java/com/xdfc/playground/adapter/in/web/rest/validator/UniqueAccountTransferAddressesValidator.java
+++ src/main/java/com/xdfc/playground/adapter/in/web/rest/validator/UniqueAccountTransferAddressesValidator.java
@@ -5,6 +5,8 @@ import com.xdfc.playground.adapter.in.web.rest.dto.account.AccountTransferDto;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+// This validator could be made more generic and useful for other cases,
+// but I need to learn more about Java for that (next steps).
 public class UniqueAccountTransferAddressesValidator implements ConstraintValidator<
     UniqueAccountTransferAddresses,
     AccountTransferDto
